12c5456b9453efc8526820d14a0db89c80014de3
Ahlul Esasjana <cakahlul@gmail.com>
2022-08-25 17:26:34 +0700

    Initial Commit

---
+++ whatsapp-settings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whatsapp-settings
+//
+//  Created by Ahlul Esasjana on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ whatsapp-settings/whatsapp_settingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsapp_settingsApp.swift
+//  whatsapp-settings
+//
+//  Created by Ahlul Esasjana on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct whatsapp_settingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
